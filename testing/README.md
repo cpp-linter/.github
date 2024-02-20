3798cb911a95f1a387ee225b73e103c79bcb948b
shenxianpeng <xianpeng.shen@gmail.com>
2024-02-20 23:24:40 +0800

    add test dummy code

---
+++ testing/README.md
@@ -0,0 +1,3 @@
+# Test reusable workflows
+
+This folder code is used for testing reusable workflow under `.github/workflows/`.
---
+++ testing/main.py
@@ -0,0 +1,8 @@
+"""test reusable workflows"""
+
+def dummy():
+    print("dummy function.")
+
+
+if "__main__" == __name__:
+    dummy()
